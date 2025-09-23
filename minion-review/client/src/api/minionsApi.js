8ee84738ce04227e80bc523bf98b8f10f3417183
BenjaminKnapp <knappben9@gmail.com>
2025-09-23 12:33:43 -0400

    Cards

--- minion-review/client/src/api/minionsApi.js
+++ minion-review/client/src/api/minionsApi.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const minionsApi = createApi({
   reducerPath: 'minionsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://minion-review-site.onrender.com/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://minion-review-site.onrender.com/api'
+  }),
   endpoints: (builder) => ({
     getMinions: builder.query({
       query: () => 'minions'
